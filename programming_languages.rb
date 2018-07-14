fe3aa4e9da61d5806777afdc494cb48ce4522a8d
Cindy Vo <yendai.vo@hotmail.com>
2018-07-14 22:26:13 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,7 +3,7 @@ def reformat_languages(languages)
   languages.each { |style, language|
     language.each { |key, value|
       new_format = value
-      new_format[:style] = [style]
+      new_format[:style] = new_format[:style].push(style)
       new_hash[key] = new_format
     }
   }
