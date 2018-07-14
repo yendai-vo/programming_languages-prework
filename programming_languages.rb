9847971d75cd3ce52dbafb7daa242f6010c2d955
Cindy Vo <yendai.vo@hotmail.com>
2018-07-14 22:33:00 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,15 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each { |style, language|
+    language.each { |key, value|
+      if(new_hash.key?(key))
+        new_hash[key][:style].push(style)
+      else
+        new_format = value
+        new_format[:style] = [style]
+        new_hash[key] = new_format
+      end
+    }
+  }
+  new_hash
 end
